78d8680aaa39afc5b44cedb8fcc447659f9f5d97
joshuamainsmith <joshuamainsmith@gmail.com>
2022-06-13 11:33:47 -0700

    Added paddle class

---
+++ Engine/Paddle.cpp
@@ -0,0 +1,51 @@
+#include "Paddle.h"
+
+Paddle::Paddle()
+{
+	pos.x = Graphics::ScreenWidth / 2;
+	pos.y = Graphics::ScreenHeight - ((Graphics::ScreenHeight / 100) * 10);
+}
+
+Paddle::Paddle(Vec2 &pos)
+	:
+	pos(pos)
+{
+}
+
+void Paddle::draw(Graphics& gfx)
+{
+	gfx.DrawRect(pos.x, pos.y, pos.x + width, pos.y + height, Colors::Cyan);
+}
+
+void Paddle::update(const Keyboard& kbd)
+{
+	if (kbd.KeyIsPressed(VK_LEFT))
+	{
+		pos.x-=speed;
+	}
+	else if (kbd.KeyIsPressed(VK_RIGHT))
+	{
+		pos.x+=speed;
+	}
+}
+
+bool Paddle::wallBounce()
+{
+
+	if (pos.x >= Graphics::ScreenWidth - width)
+		pos.x = Graphics::ScreenWidth - width;
+	else if (pos.x <= 0)
+		pos.x = 0;
+
+	return false;
+}
+
+Vec2 Paddle::getPos()
+{
+	return pos;
+}
+
+float Paddle::getwidth()
+{
+	return width;
+}
---
+++ Engine/Paddle.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "Vec2.h"
+#include "Graphics.h"
+#include "Keyboard.h"
+
+class Paddle
+{
+public:
+	Paddle();
+	Paddle(Vec2 &pos);
+	void draw(Graphics &gfx);
+	void update(const Keyboard & kbd);
+	bool wallBounce();
+	Vec2 getPos();
+	float getwidth();
+
+private:
+	Vec2 pos;
+	Vec2 vel;
+	float width = 75;
+	float height = 20;
+	float speed = 10;
+
+};
